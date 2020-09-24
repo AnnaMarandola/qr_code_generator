320ff72ea0a37720ad7e5ce6e8538ae269610c9d
AnnaMarandola <annamarandola@gmail.com>
2020-09-24 20:00:02 +0200

    refacto: directly display the pdf file and the download button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import QrCode from "./QrCode";
 import { Button, Typography, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
-import LOGO from "../assets/LogoProject.png";
 import PDF from "./PDF";
 
 const styles = (theme) => ({
@@ -29,11 +27,11 @@ const styles = (theme) => ({
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   clearButton: {
     marginTop: "2rem",
-    width: "5rem", 
+    width: "5rem",
   },
   logo: {
     width: "50%",
@@ -41,77 +39,54 @@ const styles = (theme) => ({
   },
 });
 
-const Form = ({classes}) => {
-  const [urlToConvert, setUrl] = useState ("")
-  const [convertion, setConvertion] = useState (false)
-  const [pdf, setPdf] = useState (false)
+const Form = ({ classes }) => {
+  const [urlToConvert, setUrl] = useState("");
+  const [pdf, setPdf] = useState(false);
 
   const handleChange = (e) => {
     setUrl(e.target.value);
-  }
-  const handleClick = (e) => {
-    setConvertion(true);
-  }
-  const handleClear = (e) => {
-    setUrl("")
-    setConvertion(false)
-  }
-  const handlePdf = (e) => {
-    setPdf(true)
-  }
+  };
+  const handleClear = () => {
+    setPdf(false);
+  };
+  const handlePdf = () => {
+    setPdf(true);
+  };
 
   return (
     <div className={classes.root}>
-    { !convertion &&
-    <div className={classes.root}>
-    <Typography variant="h1" className={classes.title}>QR code generator</Typography>
-      <TextField
-        label="Entrez une URL"
-        variant="outlined"
-        color="secondary"
-        className={classes.urlInput}
-        id="urlToConvert"
-        type="text"
-        value={urlToConvert}
-        onChange={handleChange}
-      />
-      <Button
-        variant="contained"
-        className={classes.generateButton}
-        onClick={handleClick}
-      >
-        Générez le QR code
-      </Button>
-      </div>
-    }
-      { convertion ? (
-        <div className={classes.homeScreen}>
-          <QrCode url={urlToConvert} />
-          <Button
-            variant="contained"
-            className={classes.clearButton}
-            color="primary"
-            onClick={handleClear}
-          >
-            Retour
-          </Button>
+      {!pdf ? 
+        <div className={classes.root}>
+          <Typography variant="h1" className={classes.title}>
+            QR code generator
+          </Typography>
+          <TextField
+            label="Entrez une URL"
+            variant="outlined"
+            color="secondary"
+            className={classes.urlInput}
+            id="urlToConvert"
+            type="text"
+            value={urlToConvert}
+            onChange={handleChange}
+          />
           <Button
             variant="contained"
             className={classes.clearButton}
             color="primary"
             onClick={handlePdf}
           >
-            Visualiser le pdf
+            générer le pdf
           </Button>
         </div>
-      ) : (
-        <div className={classes.homeScreen}>
-          <img className={classes.logo} alt="logo" src={LOGO} />
+      :
+        <div>
+          <Button onClick={handleClear}>Retour</Button>
+          <PDF />
         </div>
-      )}
-      { pdf && <PDF/>}
+      }
     </div>
-  )
-}
+  );
+};
 
 export default withStyles(styles)(Form);
--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,31 +1,20 @@
 import React from "react";
 import Pdf from "react-to-pdf";
 import QrCode from "./QrCode";
-import useQrCode from "react-qrcode-hook";
-
 import logoResto from "../assets/RestoLogo.png";
 
-const options = {
-    margin: 5,
-    scale: 7,
-    color: {
-      dark: "#031627",
-      light: "#FDFFFC",
-    },
-  };
 
 const ref = React.createRef();
 
 const PDF = (props) => {
-    const qrCode = useQrCode(`${props.url}`, options);
 
     return (
     <>
     <div ref={ref}>
-        <img src={logoResto} alt="logo resto" />
+        <img src={logoResto} width="50%" alt="logo resto" />
         <p>11 rue de la paix, 31000 Toulouse</p>
         <h1>La carte</h1>
-        <img alt="qr code" src={qrCode} />
+        <QrCode url={props.urlToConvert} />
     </div>
     <Pdf targetRef={ref} filename="qrmenu.pdf">
     {({ toPdf }) => (<button onClick={toPdf} >Télécharger le PDF</button>)}
